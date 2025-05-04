3664f76e20cd07e72cddbe7c70b243f4481cbb0a
Caryensong <caryensong@googlemail.com>
2025-05-04 18:53:39 +0200

    app-index style

--- src/app/main-content/index/index.component.ts
+++ src/app/main-content/index/index.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-index',
-  imports: [],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.scss'
+  styleUrls: ['./index.component.scss'] // <- styleUrls (nicht "styleUrl")
 })
-export class IndexComponent {
+export class IndexComponent implements AfterViewInit {
+  currentHomeIndex = 0;
+  homeImages!: NodeListOf<HTMLImageElement>;
 
+
+  ngAfterViewInit() {
+    this.homeImages = document.querySelectorAll('.home_img img');
+    const totalHomeImages = this.homeImages.length;
+
+    const showHomeImage = (index: number) => {
+      this.homeImages.forEach((img, i) => {
+        img.classList.remove('active');
+        if (i === index) {
+          img.classList.add('active');
+        }
+      });
+    };
+
+    const nextHomeImage = () => {
+      this.currentHomeIndex = (this.currentHomeIndex + 1) % totalHomeImages;
+      showHomeImage(this.currentHomeIndex);
+    };
+
+    showHomeImage(this.currentHomeIndex);
+    setInterval(nextHomeImage, 4000);
+  }
 }
