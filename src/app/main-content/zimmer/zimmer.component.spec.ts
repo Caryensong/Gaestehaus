ae0b2b8552feeea0e15ce0a27f9754f6c3aabb55
Caryensong <caryensong@googlemail.com>
2025-05-04 18:01:25 +0200

    add all component and img

---
+++ src/app/main-content/zimmer/zimmer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ZimmerComponent } from './zimmer.component';
+
+describe('ZimmerComponent', () => {
+  let component: ZimmerComponent;
+  let fixture: ComponentFixture<ZimmerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ZimmerComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ZimmerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/main-content/zimmer/zimmer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zimmer',
+  imports: [],
+  templateUrl: './zimmer.component.html',
+  styleUrl: './zimmer.component.scss'
+})
+export class ZimmerComponent {
+
+}
