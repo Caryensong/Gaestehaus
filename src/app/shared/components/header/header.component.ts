a0e80da3db3772b7d808be1b605c07185f66809f
Caryensong <caryensong@googlemail.com>
2025-05-10 11:15:41 +0200

    add new header background

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
+  standalone:true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChild('nav', { static: false }) navRef!: ElementRef;
 
+  toggleMenu() {
+    this.navRef.nativeElement.classList.toggle('active');
+  }
 }
