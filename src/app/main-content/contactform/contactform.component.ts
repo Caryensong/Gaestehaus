44dcff2a363b87ae430b3e220219cb30cf6efcff
Caryensong <caryensong@googlemail.com>
2025-06-02 14:29:30 +0200

    Added template-driven form validation using ngModel

--- src/app/main-content/contactform/contactform.component.ts
+++ src/app/main-content/contactform/contactform.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import e from 'express';
 
 @Component({
   selector: 'app-contactform',
-  imports: [],
+  imports: [CommonModule, FormsModule],
+  standalone: true,
   templateUrl: './contactform.component.html',
-  styleUrl: './contactform.component.scss'
+  styleUrl: './contactform.component.scss',
 })
 export class ContactformComponent {
+  isChecked = false;
+
+  contactData = {
+    name: '',
+    phone: '',
+    email: '',
+    message: '',
+  };
+
+  isFormValid(): boolean {
+    return (
+      (this.contactData.name.trim() !== '' || this.contactData.name === '') &&
+      (this.contactData.phone.trim() !== '' || this.contactData.phone === '') &&
+      (this.contactData.email.trim() !== '' || this.contactData.email === '') &&
+      this.isChecked
+    );
+  }
 
 }
